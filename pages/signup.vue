d45720f87bf4665dc6e549281f984342556a3dd3
shota <shouta1348@icloud.com>
2022-10-20 15:23:48 +0900

    アカウント登録処理を実装

--- pages/signup.vue
+++ pages/signup.vue
@@ -4,25 +4,68 @@
     <div class="mt48">
       <div class="mt24 form_row">
         <label class="label" for="mail">メールアドレス</label>
-        <input class="input" id="mail" type="text" />
+        <input class="input" id="mail" type="text" v-model="userMail" />
       </div>
       <div class="mt24 form_row">
         <label class="label" for="passwd">パスワード</label>
-        <input class="input" id="passwd" type="text" />
+        <input class="input" id="passwd" type="text" v-model="userPass" />
       </div>
       <div class="mt24 form_row">
         <label class="label" for="name">名前</label>
-        <input class="input" id="name" type="text" />
+        <input class="input" id="name" type="text" v-model="userName" />
       </div>
     </div>
     <div class="mt48">
-      <button class="btn btn--blue">アカウント登録</button>
+      <button
+        class="btn btn--blue"
+        @click="createUser"
+        :disabled="!(userMail && userPass && userMail)"
+      >
+        アカウント登録
+      </button>
     </div>
     <div class="mt48">
       <p class="note">既にアカウントをお持ちの方は<a href="">こちら</a></p>
     </div>
   </div>
 </template>
+
+<script>
+import firebase from "~/plugins/firebase";
+const db = firebase.firestore();
+
+export default {
+  data() {
+    return {
+      userMail: "",
+      userName: "",
+      userPass: "",
+    };
+  },
+  methods: {
+    createUser() {
+      if (
+        this.userMail === "" ||
+        this.userName === "" ||
+        this.userPass === ""
+      ) {
+        return;
+      }
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.userMail, this.userPass)
+        .then(
+          db.collection("users").doc(this.userName).set({
+            name: this.userName,
+          })
+        )
+        .catch((error) => {
+          alert("error");
+        });
+    },
+  },
+};
+</script>
 
 <style scoped>
 .main {
